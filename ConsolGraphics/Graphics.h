d72a92b1b5600e26a28d30c26127d955ddd2c6d9
f4ilurebydesign <50153431+f4ilurebydesign@users.noreply.github.com>
2019-05-02 22:12:02 +0300

    Update Graphics.h

--- ConsolGraphics/Graphics.h
+++ ConsolGraphics/Graphics.h
@@ -7,52 +7,51 @@ namespace core { namespace cGraphics {
 #define MAX_X 40
 #define MAX_Y 40
 
-		char map[MAX_Y][MAX_X];
+	char map[MAX_Y][MAX_X];
 
-		class BaseGraphics {
-		public:
-			virtual void draw() = 0;
-		};
+	class BaseGraphics {
+	public:
+		virtual void draw() = 0;
+	};
 
-		class Map : public BaseGraphics {
-		public:
-			Map() {}
-			void init() {
-				for (int y = 0; y < MAX_Y; y++) {
-					for (int x = 0; x < MAX_X; x++) {
-						map[y][x] = ' ';//рамка
-						map[0][x] = '-';
-						map[39][x] = '-';
-						map[y][0] = '-';
-						map[y][39] = '-';
-					}
+	class Map : public BaseGraphics {
+	public:
+		Map() {}
+		void init() {
+			for (int y = 0; y < MAX_Y; y++) {
+				for (int x = 0; x < MAX_X; x++) {
+					map[y][x] = ' ';//рамка
+					map[0][x] = '-';
+					map[39][x] = '-';
+					map[y][0] = '-';
+					map[y][39] = '-';
 				}
 			}
-			void draw() {
-				for (int y = 0; y < MAX_Y; y++) {
-					for (int x = 0; x < MAX_X; x++) {
-						std::cout << map[y][x];
-					}
-					std::cout << std::endl;
+		}
+		void draw() {
+			for (int y = 0; y < MAX_Y; y++) {
+				for (int x = 0; x < MAX_X; x++) {
+					std::cout << map[y][x];
 				}
+				std::cout << std::endl;
 			}
-		};
-
+		}
+	};
 		class Line : public BaseGraphics {
-		private:
-			int x0, y0, x1, y1;
-		public:
-			Line(int x0, int y0, int x1, int y1) {
-				this->x0 = x0;
-				this->y0 = y0;
-				this->x1 = x1;
-				this->y1 = y1;
-			}
-			void draw() {
-				// - | / \
-				
-			}
-		};
+	private:
+		int x0, y0, x1, y1;
+	public:
+		Line(int x0, int y0, int x1, int y1) {
+			this->x0 = x0;
+			this->y0 = y0;
+			this->x1 = x1;
+			this->y1 = y1;
+		}
+		void draw() {
+			// - | / \
+			
+		}
+	};
 
-	}
-}
+
+} }
